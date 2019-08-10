89dbc009fa558e52f398293f13031adfefb2bb42
Pankaj Kumar <pankaj14061998@gmail.com>
2019-08-10 17:44:44 +0530

    Add files via upload

---
+++ automation_driving.py
@@ -0,0 +1,138 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Aug  9 12:32:32 2019
+
+@author: PankajKumar
+"""
+#importing packages
+import argparse
+import os
+import matplotlib.pyplot as plt
+from matplotlib.pyplot import imshow
+import scipy.io
+import scipy.misc
+import numpy as np
+import pandas as pd
+import PIL
+import tensorflow as tf
+from keras import backend as K
+from keras.layers import Input, Lambda, Conv2D
+from keras.models import load_model, Model
+from yolo_utils import read_classes, read_anchors, generate_colors, preprocess_image, draw_boxes, scale_boxes
+from yad2k.models.keras_yolo import yolo_head, yolo_boxes_to_corners, preprocess_true_boxes, yolo_loss, yolo_body
+
+# yolo_filter_boxes
+
+def yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold = .6):
+    
+    # Step 1: Compute box scores
+    box_scores = box_confidence * box_class_probs
+    
+    # Step 2: Find the box_classes thanks to the max box_scores, keep track of the corresponding score
+    box_classes = K.argmax(box_scores, axis = -1)
+    box_class_scores = K.max(box_scores, axis = -1)
+    
+    
+    # Step 3: Create a filtering mask based on "box_class_scores" by using "threshold". The mask should have the
+    # same dimension as box_class_scores, and be True for the boxes you want to keep (with probability >= threshold)
+    filtering_mask = box_class_scores > threshold
+    
+    # Step 4: Apply the mask to scores, boxes and classes
+    scores = tf.boolean_mask(box_class_scores, filtering_mask)
+    boxes = tf.boolean_mask(boxes, filtering_mask)
+    classes = tf.boolean_mask(box_classes, filtering_mask)
+    
+    return scores, boxes, classes
+
+
+# yolo_non_max_suppression
+
+def yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_threshold = 0.5):
+     
+    max_boxes_tensor = K.variable(max_boxes, dtype='int32')     # tensor to be used in tf.image.non_max_suppression()
+    K.get_session().run(tf.variables_initializer([max_boxes_tensor])) # initialize variable max_boxes_tensor
+    
+    # Use tf.image.non_max_suppression() to get the list of indices corresponding to boxes you keep
+   
+    nms_indices = tf.image.non_max_suppression(boxes, scores, max_boxes_tensor, iou_threshold)
+    
+    
+    # Use K.gather() to select only nms_indices from scores, boxes and classes
+    
+    scores = K.gather(scores, nms_indices)
+    boxes = K.gather(boxes, nms_indices)
+    classes = K.gather(classes, nms_indices)
+    
+    
+    return scores, boxes, classes
+
+
+# GRADED FUNCTION: yolo_eval
+
+def yolo_eval(yolo_outputs, image_shape = (720., 1280.), max_boxes=10, score_threshold=.6, iou_threshold=.5):
+    
+    
+    # Retrieve outputs of the YOLO model (≈1 line)
+    box_confidence, box_xy, box_wh, box_class_probs = yolo_outputs
+
+    # Convert boxes to be ready for filtering functions 
+    boxes = yolo_boxes_to_corners(box_xy, box_wh)
+
+    # Use yolo filter boxes function  to perform Score-filtering with a threshold of score_threshold
+    scores, boxes, classes = yolo_filter_boxes(box_confidence, boxes, box_class_probs, threshold = .6)
+    
+    # Scale boxes back to original image shape.
+    boxes = scale_boxes(boxes, image_shape)
+
+    # Use function Non-max suppression with a threshold of iou_threshold 
+    scores, boxes, classes = yolo_non_max_suppression(scores, boxes, classes, max_boxes = 10, iou_threshold = 0.5)
+    
+    
+    return scores, boxes, classes
+
+
+#creating a session to start our graph
+sess = K.get_session()
+
+#The car detection dataset has 720x1280 images, which we've pre-processed into 608x608 images.
+class_names = read_classes("coco_classes.txt")
+anchors = read_anchors("yolo_anchors.txt")
+image_shape = (720., 1280.)    
+
+#Loading a pretrained model- yolo.h5
+yolo_model = load_model("yolo.h5")
+
+#summary of the layers our model yolo.h5 contains.
+yolo_model.summary()
+
+#Converting output of the model to usable bounding box tensors
+yolo_outputs = yolo_head(yolo_model.output, anchors, len(class_names))
+
+#Filtering boxes
+scores, boxes, classes = yolo_eval(yolo_outputs, image_shape)
+
+#Running the graph on an image
+def predict(sess, image_file):
+   
+    # Preprocess your image
+    image, image_data = preprocess_image("images/" + image_file, model_image_size = (608, 608))
+
+    # Running the session.
+    out_scores, out_boxes, out_classes = sess.run([scores, boxes, classes], feed_dict={yolo_model.input: image_data, K.learning_phase():0})
+    
+    # Print predictions info
+    print('Found {} boxes for {}'.format(len(out_boxes), image_file))
+    # Generate colors for drawing bounding boxes.
+    colors = generate_colors(class_names)
+    # Draw bounding boxes on the image file
+    draw_boxes(image, out_scores, out_boxes, out_classes, class_names, colors)
+    # Save the predicted bounding box on the image
+    image.save(os.path.join("out", image_file), quality=90)
+    # Display the results in the notebook
+    output_image = scipy.misc.imread(os.path.join("out", image_file))
+    imshow(output_image)
+    
+    return out_scores, out_boxes, out_classes
+
+
+out_scores, out_boxes, out_classes = predict(sess, "City-car.jpg")
